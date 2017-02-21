65de8331c45dfaba6e3af77e9cf0f20e6cb42318
Qiao Zhang <qiao.zhang.2015@gmail.com>
2017-02-21 11:13:46 -0500

    make Coordinate and Location Equatable

--- ToDo/Location.swift
+++ ToDo/Location.swift
@@ -8,12 +8,12 @@
 
 import Foundation
 
-struct Coordinate {
+struct Coordinate: Equatable {
   let latitude: Double
   let longitude: Double
 }
 
-struct Location {
+struct Location: Equatable {
   let name: String
   let coordinate: Coordinate?
 
@@ -21,4 +21,16 @@ struct Location {
     self.name = name
     self.coordinate = coordinate
   }
+}
+
+func == (left: Coordinate, right: Coordinate) -> Bool {
+  if abs(left.latitude - right.latitude) > DBL_EPSILON { return false }
+  if abs(left.longitude - right.longitude) > DBL_EPSILON { return false }
+  return true
+}
+
+func == (left: Location, right: Location) -> Bool {
+  if left.name != right.name { return false }
+  if left.coordinate != right.coordinate { return false }
+  return true
 }
--- ToDoTests/LocationTests.swift
+++ ToDoTests/LocationTests.swift
@@ -9,28 +9,20 @@
 import XCTest
 @testable import ToDo
 
-class CoordinateLocationTests: XCTestCase {
+class LocationTests: XCTestCase {
 
   override func setUp() {
     super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
   }
     
   override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     super.tearDown()
   }
 
+  // MARK: Tests for Location
   func test_LocationInit_WhenGivenName_SetsName() {
     let location = Location(name: "Foo")
     XCTAssertEqual(location.name, "Foo")
-  }
-
-  func test_CoordinateInit_TakesLatAndLongAndSetsThem() {
-    let coord = Coordinate(latitude: 1.3, longitude: 3.14)
-    XCTAssertEqualWithAccuracy(coord.latitude, 1.3, accuracy: 0.0001)
-    XCTAssertEqualWithAccuracy(coord.longitude, 3.14, accuracy: 0.0001)
-
   }
 
   func test_LocationInit_WhenGivenCoordinate_SetsCoordinate() {
@@ -39,9 +31,81 @@ class CoordinateLocationTests: XCTestCase {
     XCTAssertNotNil(location.coordinate)
     let locoord = location.coordinate!
     XCTAssertEqualWithAccuracy(locoord.latitude,
-                               coordinate.latitude, accuracy: 0.0001)
+                               coordinate.latitude, accuracy: DBL_EPSILON)
     XCTAssertEqualWithAccuracy(locoord.longitude,
-                               coordinate.longitude, accuracy: 0.0001)
+                               coordinate.longitude, accuracy: DBL_EPSILON)
+  }
+  
+  func test_EqualLocations_AreEqual() {
+    let coord = Coordinate(latitude: 1.234, longitude: 5.678)
+    let first = Location(name: "Foo", coordinate: coord)
+    let second = Location(name: "Foo", coordinate: coord)
+    XCTAssertEqual(first, second)
   }
 
+  func test_Locations_WhenNamesDiffer_AreNotEqual() {
+    let latlong = (1.234, 5.678)
+    performNotEqualLocationsTestWith(
+      firstName: "Foo", secondName: "Bar",
+      firstLongLat: latlong, secondLongLat: latlong)
+  }
+
+  func test_Locations_WhenCoordinatesDiffer_AreNotEqual() {
+    performNotEqualLocationsTestWith(
+      firstName: "Foo", secondName: "Foo",
+      firstLongLat: (1.234, 4.5678), secondLongLat: (1.234, 5.876))
+  }
+  
+  func test_Locations_WhenOnlyOneHasCoordinate_AreNotEqual() {
+    performNotEqualLocationsTestWith(
+      firstName: "Foo", secondName: "Foo",
+      firstLongLat: (0.3857, 7.28492), secondLongLat: nil)
+  }
+
+  // MARK: Tests for Coordiante
+  func test_Coordinate_TakesLatitudeAndLongitude_AndSetsThem() {
+    let coord = Coordinate(latitude: 1.3, longitude: 3.14)
+    XCTAssertEqualWithAccuracy(coord.latitude, 1.3, accuracy: DBL_EPSILON)
+    XCTAssertEqualWithAccuracy(coord.longitude, 3.14, accuracy: DBL_EPSILON)
+
+  }
+  
+  func test_EqualCoordinates_AreEqual() {
+    let first = Coordinate(latitude: 1.1124, longitude: 3.4954)
+    let second = Coordinate(latitude: 1.1124, longitude: 3.4954)
+    XCTAssertEqual(first, second)
+  }
+
+  func test_Coordinates_WhenLatitudesDiffer_AreNotEqual() {
+    let first = Coordinate(latitude: 1.1, longitude: 3.4954)
+    let second = Coordinate(latitude: 1.1124, longitude: 3.4954)
+    XCTAssertNotEqual(first, second)
+  }
+
+  func test_Coordinates_WhenLongitudesDiffer_AreNotEqual() {
+    let first = Coordinate(latitude: 1.1124, longitude: 3.4954333)
+    let second = Coordinate(latitude: 1.1124, longitude: 3.4954332)
+    XCTAssertNotEqual(first, second)
+  }
+  
+  // MARK: Helpers
+  private func performNotEqualLocationsTestWith(
+    firstName: String, secondName: String,
+    firstLongLat: (Double, Double)?, secondLongLat: (Double, Double)?,
+    line: UInt = #line) {
+    
+    var coord1: Coordinate? = nil, coord2: Coordinate? = nil
+    if let (lat, long) = firstLongLat {
+      coord1 = Coordinate(latitude: lat, longitude: long)
+    }
+    if let (lat, long) = secondLongLat {
+      coord2 = Coordinate(latitude: lat, longitude: long)
+    }
+    
+    assert(firstName != secondName || coord1 != coord2)
+    
+    let first = Location(name: firstName, coordinate: coord1)
+    let second = Location(name: secondName, coordinate: coord2)
+    XCTAssertNotEqual(first, second, line: line)
+  }
 }
